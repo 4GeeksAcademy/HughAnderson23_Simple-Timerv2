576aa8aa775a841a59f6dbb9609319b52ea78a46
Hugh Anderson <144385898+HughAnderson23@users.noreply.github.com>
2023-12-01 21:53:15 +0000

    working Timer

--- src/js/index.js
+++ src/js/index.js
@@ -3,16 +3,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 import PropTypes from 'prop-types';
 // include your styles into the webpack bundle
-
+import './icons.js'
 
 import "../styles/index.css";
+
 
 //import your own components
 
 function SimpleCounter(props){
     return (<div className="bigCounter">
         <div className="calendar">
-        <i class="fa-regular fa-clock"></i>
+        <i className="fa-regular fa-clock"></i>
         </div>
         <div className="four">{props.digitFour %10}</div>
         <div className="three">{props.digitThree %10}</div>
@@ -38,9 +39,10 @@ setInterval(function(){
     console.log(four, three, two, one);
 
     counter++;
-},1000);
+
 //render your react application
 ReactDOM.render(
-    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={Four} />,
+    <SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />,
     document.querySelector("#app")
 )
+},1000);
